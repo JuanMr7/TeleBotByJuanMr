07d9050b06fd5bb107958b91abf22b666278369e
JuanMr <75852505+JuanMr7@users.noreply.github.com>
2021-03-07 00:11:38 -0500

    Update vuelosDisponibles.py

--- vuelosDisponibles.py
+++ vuelosDisponibles.py
@@ -14,11 +14,11 @@ class Vuelo:
 class Aeropuerto:
     def __init__(self, datos_json):
         self.nombre = datos_json["nombre"]
-        self.ciud = datos_json["ciud"]
+        self.iata = datos_json["iata"]
         self.pais = datos_json["pais"]
         self.provincia = datos_json["provincia"]
         self.ciudad = datos_json["ciudad"]
-        self.vuelos = []
+        self.vuelosDisponibles = []
 
     def generar_vuelos(self, aeropuertos, min_vuelos = 4, max_vuelos = 8):
         for i in range( 0, rd.randrange(min_vuelos, max_vuelos)):
@@ -30,10 +30,10 @@ class Aeropuerto:
             hora = datetime.time(rd.randrange(0, 24), rd.randrange(0, 60))
             fecha = datetime.date.today() + datetime.timedelta(days= rd.randrange(1, 16))
             vuelo = Vuelo(aeropuerto, hora, fecha, self)
-            self.vuelos.append(vuelo)
+            self.vuelosDispobibles.append(vuelo)
 
     def eliminar_vuelos(self):
-        for vuelo in self.vuelos:
+        for vuelo in self.vuelosDisponibles:
             print("Aeropuerto: " + vuelo.aeropuerto_destino)
 
     def __repr__(self):
